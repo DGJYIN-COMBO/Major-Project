a4cb393f86ff7b7320547d73a8a691e61427d4c8
jayden middleton <jaydenmiddleton2269@gmail.com>
2014-05-06 14:26:12 +1000

    More work on the program, word changes, adding of buttons

--- WindowsApplication1/WindowsApplication1/Form3.vb
+++ WindowsApplication1/WindowsApplication1/Form3.vb
@@ -1,12 +1,6 @@
 ﻿Public Class Form3
 
-    Private Sub Chart1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
-
-    End Sub
-
-    Private Sub ComboBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs)
-
-    End Sub
+    
     Sub AddAxis(ByVal label As String, ByVal value As Double)
 
         Chart1.Series(0).Points.AddY(value)
@@ -14,18 +8,19 @@
 
     End Sub
 
+    Sub CalcuLateTotal()
+
+
+
+    End Sub
+
     Private Sub Form3_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
+        AddAxis("Dairy", TxtDairy.Text)
 
     End Sub
 
-
-
-    Private Sub Chart1_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Chart1.Click
-
-    End Sub
-
-    Private Sub TextBox15_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox15.TextChanged
+    Private Sub SaveBudgetBTN_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SaveBudgetBTN.Click
 
     End Sub
 End Class
